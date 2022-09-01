025d286309d3c8b915fa686c685a995025245be7
Davi Nacaratti <88853504+nacaratti@users.noreply.github.com>
2022-09-01 17:01:23 -0300

    Add files via upload

---
+++ FDTD_1D_matriz_diagonal.m
@@ -0,0 +1,69 @@
+clear all; clc;
+
+ep0 = 1;                      % Permissividade
+mu0 = 1;                      % Permeabilidade magnetica
+%c = 1/sqrt(ep0*mu0);                  % Velocidade da luz no vacuo
+c=1;
+lambda0 = 1.064e-6;                   % Comprimento de onda laser Yb
+n = 1.45;                             % Indice de refracao            
+L = 30e-6;                            % Comprimento em z
+N = 100;                              % Numero de series em z
+ep = ones(N,1);                       % Vetor de Permissividade         
+mu = ones(N,1);                       % Vetor de Permeabilidade magnetica      
+dz = 0.1;                             % Incremento em z
+z = (0:(N-1))*dz;                     % Vetor z
+dt = 0.01;                             % Incremento t              
+Z = sqrt(mu0/ep0);                    % Impedancia E'=E/Z
+Hy = zeros(N,1);     
+Ex = zeros(N,1);
+
+if (lambda0/dz) >= (10*n)
+    fprintf('Condi??o N=(lambda/h)>=10n satisfeita!!');
+else
+    fprintf('Condi??o N=(lambda/h)>=10n NAO satisfeita!!');
+end
+
+% for i = 1:20
+%     ep(i,1) = 1.0; 
+% end  
+%   for i = 20+1:50
+%       ep(i,1) = 2.5; 
+%   end  
+% figure(1);
+% plot(ep);
+% title('Perfil permissividade');
+
+Df = spdiags([-mu mu],0:1,N,N)/dz;
+Df(N,1) = Df(1,2);
+Db = spdiags([-ep ep],-1:0,N,N)/dz; 
+Db(1,N) = Db(2,1);
+figure(2);
+subplot(1,2,1);
+imagesc(Df);
+subplot(1,2,2);
+imagesc(Db);
+
+% Pulso Gaussiano
+Ex = exp(-((z'-5)).^2);
+
+Nt = 1000;      
+  for i=1:Nt
+
+      Hy = Hy - Df*dt*Ex;
+  
+      Ex = Ex - Db*dt*Hy;
+      figure(3);
+      subplot(2,1,1);
+      plot(z,Ex,'b');           
+      %axis([0 N -1 1]);
+      grid on
+      xlabel('z');
+      ylabel('Ex');
+      subplot(2,1,2);
+      plot(z,Hy,'r');      
+      grid on
+      %axis([0 N -1 1]);
+      xlabel('z');
+      ylabel('Hy');
+      drawnow
+  end
